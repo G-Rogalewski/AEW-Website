7c2b7cca408d9b4d13ed91fc61d0d0ae94fb3255
Gustavo Rogalewski <gustavo.rogalewski@bol.com.br>
2022-07-18 12:49:47 -0300

    redirect of champions

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("button-one").onclick = function () {
+    location.href = "https://www.allelitewrestling.com/post/aew-x-njpw-forbidden-door-results";
+};
+
+document.getElementById("button-two").onclick = function () {
+    location.href = "https://www.allelitewrestling.com/post/aew-rampage-results-for-june-24-2022";
+};
+
+document.getElementById("button-three").onclick = function () {
+            location.href = "https://www.allelitewrestling.com/post/aew-dynamite-results-for-june-22-2022";
+};
